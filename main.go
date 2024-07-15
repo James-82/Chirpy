77c68d46eab3f889f488f3b0e14bedb651620bb9
James-82 <jamesemployed@gmail.com>
2024-07-15 10:06:05 +0800

    0: Updated main.go

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+    "net/http"
     "fmt"
 )
 
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello, world!")
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    // Creating a new instance of http.ServeMux using the http.NewServeMux function
+    mux := http.NewServeMux()
+
+    // Registering a handler function with the ServeMux instance
+    mux.HandleFunc("/", mainHandler)
+
+    // Creating and configuring an HTTP server with the ServeMux instance as the handler
+    server := &http.Server{
+        Addr:    "localhost:8080",
+        Handler: mux,
+    }
+
+    // Starting the server
+    fmt.Println("Starting server on :8080")
+    server.ListenAndServe()
 }
