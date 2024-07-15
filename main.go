9370b63656991da630dac855df4755a9113cbd4f
James-82 <jamesemployed@gmail.com>
2024-07-15 10:53:43 +0800

    1: Server is up

--- main.go
+++ main.go
@@ -1,28 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "net/http"
 )
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+// Custom handler that always returns a 404 status
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+    http.NotFound(w, r)
 }
 
 func main() {
-	// Creating a new instance of http.ServeMux using the http.NewServeMux function
-	mux := http.NewServeMux()
+    // Creating a new instance of http.ServeMux using the http.NewServeMux function
+    mux := http.NewServeMux()
 
-	// Registering a handler function with the ServeMux instance
-	mux.HandleFunc("/", mainHandler)
+    // Register the custom 404 handler for all paths
+    mux.HandleFunc("/", notFoundHandler)
 
-	// Creating and configuring an HTTP server with the ServeMux instance as the handler
-	server := &http.Server{
-		Addr:    "localhost:8080",
-		Handler: mux,
-	}
+    // Creating and configuring an HTTP server with the ServeMux instance as the handler
+    server := &http.Server{
+        Addr:    "localhost:8080",
+        Handler: mux,
+    }
 
-	// Starting the server
-	fmt.Println("Starting server on :8080")
-	server.ListenAndServe()
+    // Starting the server
+    server.ListenAndServe()
 }
